1bd9eb85c30f82d11ccd53b55c12b61104caba9d
qqa00165 <qqa00165@gmail.com>
2017-09-30 15:01:04 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int a;
+        int b;
+        a=in.nextInt();
+        b=in.nextInt();
+        System.out.println(a/2.54);
+        System.out.println(b/0.454);
+
+	// write your code here
+    }
+}
